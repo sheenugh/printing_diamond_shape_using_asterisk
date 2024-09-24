dd819fd27b3e6976e4aa582930fd811686cbee62
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-24 17:24:44 +0800

    Altering pseudo code and adding def print_diamond.

--- printing_diamond_shape_using_asterisk.py
+++ printing_diamond_shape_using_asterisk.py
@@ -1,5 +1,10 @@
 # ========== PSEUDO CODE ==========
 # || ACTUAL CODE ||
-# - Initializing a variable.
+# - Def function print_diamond
+def print_diamond(n):
+    if n % 2 == 0:
+        return "Please return an odd integer."
+
 # - Loop syntax
 # - Printing the result
+
