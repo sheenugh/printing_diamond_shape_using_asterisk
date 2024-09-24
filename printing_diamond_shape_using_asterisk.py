0d19189d3c79875a8d7afe8b12cad28c9cf51518
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-24 17:33:31 +0800

    Done.

--- printing_diamond_shape_using_asterisk.py
+++ printing_diamond_shape_using_asterisk.py
@@ -5,6 +5,11 @@ def print_diamond(n):
     if n % 2 == 0:
         return "Please return an odd integer."
 
-# - Loop syntax
-# - Printing the result
+    # - Loop syntax
+    for i in range(1, n+1):
+        if i <= n // 2 +1:
+            print(" " * (n //2-i+1) + "*" * (2 * i -1))
+        else:
+            print(" " * (i - n // 2 - 1) + "*" * (2 * (n - i + 1) - 1))
 
+print_diamond(5)
